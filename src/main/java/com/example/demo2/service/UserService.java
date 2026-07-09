fa8e6613929a47d027f95e7d59295278aa3b41bb
nadeemshaikk <nadeemshaiksalesforce@gmail.com>
2026-07-09 18:30:53 +0530

    Added Spring Log

--- src/main/java/com/example/demo2/service/UserService.java
+++ src/main/java/com/example/demo2/service/UserService.java
@@ -5,23 +5,42 @@ import com.example.demo2.model.User;
 import com.example.demo2.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @Service
 public class UserService {
 
+    //adding Logger
+
+    private static final Logger logger =
+            LoggerFactory.getLogger(UserService.class);
+
     @Autowired
     private UserRepo userRepo;
 
     public User saveUser(User user){
+        logger.info("Saving user"); //log
         return userRepo.save(user);
     }
     public List<User> getAllUsers(){
+        logger.info("Fetching all users"); //log
         return userRepo.findAll();
     }
     public User getUserById(Long id){
-        return userRepo.findById(id).orElseThrow(() -> new UserNotFoundException("User not found with id: "+ id));
+        logger.info("Fetching user with id {}", id);
+
+        return userRepo.findById(id)
+
+                .orElseThrow(() -> {
+
+                    logger.error("User not found with id {}", id);
+
+                    return new UserNotFoundException("User not found with id : "+id);
+
+                });
     }
 //    public User updateUser(Long id, User user){
 //        User existing = userRepo.findById(id).orElse(null);
